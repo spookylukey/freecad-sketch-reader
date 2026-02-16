c68f01bbed275270cdfd3fe8d89bcc38666f6286
Luke Plant <luke@lukeplant.me.uk>
2026-02-16 16:04:08 +0000

    Add release script

---
+++ release.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Tests to ensure we don't really break something:
+uv sync
+pre-commit run --all --all-files || exit 1
+pytest || exit 1
+pyright || exit 1
+
+umask 000
+rm -rf build dist
+git ls-tree --full-tree --name-only -r HEAD | xargs chmod ugo+r
+
+uv build --sdist --wheel || exit 1
+uv publish  || exit 1
+
+VERSION=$(uv version --short | tr -d '\n') || exit 1
+
+git tag $VERSION || exit 1
+git push || exit 1
+git push --tags || exit 1
